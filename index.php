9fc9cd44f557fff11859d52ea95804e076c8e1de
Stephen Benjamin <stephen@bitbin.de>
2017-09-07 18:22:01 -0400

    Fix indentation

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 $quotes = [
-        "Only when we have become nonviolent towards all life will we have learned to live well with others. - Cesar Chavez",
-        "Better than a thousand hollow words is one word that brings peace. - Buddha, from the Dhammapada",
-        "One of the greatest opportunities to live our values - or betray them - lies in the food we put on our plates. - Jonathan Safran Foer, Eating Animals",
-        "Think occasionally of the suffering of which you spare yourself the sight. - Albert Schweitzer"
-]; 
+    "Only when we have become nonviolent towards all life will we have learned to live well with others. - Cesar Chavez",
+    "Better than a thousand hollow words is one word that brings peace. - Buddha, from the Dhammapada",
+    "One of the greatest opportunities to live our values - or betray them - lies in the food we put on our plates. - Jonathan Safran Foer, Eating Animals",
+    "Think occasionally of the suffering of which you spare yourself the sight. - Albert Schweitzer"
+];
 ?>
 
 <!DOCTYPE html>
@@ -28,10 +28,10 @@ $quotes = [
                 <hr />
 
                 <blockquote>
-                <p><?php
-                        echo $quotes[array_rand($quotes)];
-                ?>
-                </p>
+                    <p><?php
+                            echo $quotes[array_rand($quotes)];
+                       ?>
+                    </p>
                 </blockquote>
 
                 <hr />
@@ -53,7 +53,7 @@ $quotes = [
                 for a number of years. I'm currently employed by Red Hat, but
                 on a leave of absence while I finish the last few requirements for my
                 Master's degree.</p>
-</div>
-</div>
-</body>
+            </div>
+        </div>
+    </body>
 </html>
