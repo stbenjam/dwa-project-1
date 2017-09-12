fae53ffe18433e25a1e2cb053fa0e221b80801af
Stephen Benjamin <stephen@bitbin.de>
2017-09-12 13:43:58 -0400

    Separate logic

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
-<?php
-$quotes = [
-    "Only when we have become nonviolent towards all life will we have learned to live well with others. - Cesar Chavez",
-    "Better than a thousand hollow words is one word that brings peace. - Buddha, from the Dhammapada",
-    "One of the greatest opportunities to live our values - or betray them - lies in the food we put on our plates. - Jonathan Safran Foer, Eating Animals",
-    "Think occasionally of the suffering of which you spare yourself the sight. - Albert Schweitzer"
-];
-?>
-
+<?php require('quoteLogic.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -28,10 +20,7 @@ $quotes = [
                 <hr />
 
                 <blockquote>
-                    <p><?php
-                            echo $quotes[array_rand($quotes)];
-                       ?>
-                    </p>
+                    <p><?php echo $randomQuote; ?></p>
                 </blockquote>
 
                 <hr />
---
+++ quoteLogic.php
@@ -0,0 +1,12 @@
+<?php
+
+$quotes = [
+    "Only when we have become nonviolent towards all life will we have learned to live well with others. - Cesar Chavez",
+    "Better than a thousand hollow words is one word that brings peace. - Buddha, from the Dhammapada",
+    "One of the greatest opportunities to live our values - or betray them - lies in the food we put on our plates. - Jonathan Safran Foer, Eating Animals",
+    "Think occasionally of the suffering of which you spare yourself the sight. - Albert Schweitzer"
+];
+
+$randomQuote = $quotes[array_rand($quotes)];
+
+?>
